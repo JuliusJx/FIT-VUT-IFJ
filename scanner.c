e14e1b1251288e547c740ac23753aa8257d15dff
JuliusJ <juliusj@DESKTOP-D10ALBB.localdomain>
2021-10-13 20:27:48 +0200

    scanner.c created

---
+++ scanner.c
@@ -0,0 +1,25 @@
+/*
+ * File: scanner.c
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal
+ * Year: 2021
+ */
+
+#include "stdio.h"
+#include "scanner.h"
+
+token *GetToken(){
+    token NewToken;
+    unsigned FSM_State = FSM_Start;
+    
+    while(FSM_State != FSM_End){
+        switch (FSM_State){
+            case FSM_Start
+            default:
+        }
+    }
+
+
+}
+
+// --End of file--
