0ff316b85742aab7a4010cc4d5d59fb1a0f51c22
JuliusJ <juliusj@DESKTOP-D10ALBB.localdomain>
2021-10-13 20:27:10 +0200

    changes in FSM states

--- scanner.h
+++ scanner.h
@@ -9,15 +9,17 @@
 #ifndef scanner_h_
 #define scanner_h_
 
-#define FSM_Read 0
-#define FSM_StrArr 1      // "
-#define FSM_ID 2          // _
-#define FSM_IDorKey 3     // a/A..z/Z
-#define FSM_Number 4      // 0..9
-#define FSM_Float 5       // ./e
-#define FSM_Operator 6    // Arithmetic/Relation operators
-#define FSM_Comment 7     // --
-#define FSM_MultiLineC 8  // --[[
+#define FSM_Start 0
+#define FSM_StrArr 1        // "
+#define FSM_ID 2            // _
+#define FSM_IDorKey 3       // a/A..z/Z
+#define FSM_Number 4        // 0..9
+#define FSM_Float 5         // ./e
+#define FSM_Operator 6      // Arithmetic/Relation operators
+#define FSM_Comment 7       // --
+#define FSM_MultLComment 8  // --[[
+#define FSM_CheckKeyWords 9
+#define FSM_End 10
 
 typedef struct token{
     char *content;
