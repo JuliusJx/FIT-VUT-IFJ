a2d1929fa9aea46c5126926d8bc903e091422761
JuliusJ <juliusj@DESKTOP-D10ALBB.localdomain>
2021-10-22 20:38:18 +0200

    obsolete code removed

--- scanner.c
+++ scanner.c
@@ -1,5 +1,6 @@
 /*
  * File: scanner.c
+ * c file for lexical analysis
  * Subject: IFJ
  * Authors: Jakub Julius Smykal
  * Year: 2021
@@ -12,51 +13,13 @@ token *GetToken(){
     token *NewToken;
     unsigned FSM_State = FSM_Start;
     char c;
+    short done = 0;
 
-    while(FSM_State != FSM_End){
+    while(!done){
         c = getc(stdin);
         switch (FSM_State){
             case FSM_Start:
-                if(c == '"')
-                    FSM_State = FSM_String;
-                else if(c == '_')
-                    FSM_State = FSM_ID;
-                else if(((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (c <= 'Z')))
-                    FSM_State = FSM_IDorKey;
-                else if((c >= '0') && (c <= '9'))
-                    FSM_State = FSM_Int;
-                else if((c == '(') || (c == ')') || (c == ','))
-                    FSM_State = FSM_Separator;
-                else if((c == '#') || (c == ':') || (c == '+') || (c == '*') 
-                        || (c == '/') || (c == '<') || (c == '=') || (c == '>'))
-                    FSM_State = FSM_Operator;
-                else if(c == '-'){
-                    //checks if next character is -, if yes it is comment
-                    char c2 = getc(stdin);
-                    if(c2 == '-'){
-                        //Gets next 2 characters and checks if it is multi line comment
-                        c2 = getc(stdin);
-                        char c3 = getc(stdin);
-                        if((c2 == '[')&&(c3 == '[')){
-                            FSM_State = FSM_MultLComment;
-                        }
-                        else{
-                            //Returns characters if it is not multiline comment
-                            //otherwise could cause problems with short comments
-                            ungetc(c3, stdin);
-                            ungetc(c2, stdin);
-                            FSM_State = FSM_Comment;
-                        }
-                    }
-                    else{
-                        FSM_State = FSM_Operator;
-                        ungetc(c2, stdin);
-                    }
-                }
-                else if(c == EOF)
-                    FSM_State = FSM_End;
-            default:
-                break;
+
         }
     }
     return NewToken;
