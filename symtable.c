8318e5347e4ea3075c6304c39a47ee5cfa009d6e
JuliusJ <juliusj@DESKTOP-D10ALBB.localdomain>
2021-11-17 20:59:04 +0100

    symtable.h and symtable.c files created

---
+++ symtable.c
@@ -0,0 +1,11 @@
+/*
+ * File: symtable.h
+ * Header file for symtable.c
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+
+
+// --End of file--
---
+++ symtable.h
@@ -0,0 +1,16 @@
+/*
+ * File: symtable.h
+ * Header file for symtable.c
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+#ifndef stack_h_
+#define stack_h_
+
+
+
+#endif
+
+// --End of file--
