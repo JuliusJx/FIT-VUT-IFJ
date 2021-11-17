62353d334298f209cdd3ff28ef7d8dfb79090c69
JuliusJx <jakubjsmykal@gmail.com>
2021-11-17 21:20:00 +0100

    Fixed structure names and missing includes

--- stack.h
+++ stack.h
@@ -9,15 +9,18 @@
 #ifndef stack_h_
 #define stack_h_
 
+#include <stdio.h>
+#include <stdbool.h>
+
 //Structure for items in stack
 typedef struct  item{
     int value; //token type / scope index
-    item *next;
+    StackItem *next;
 }StackItem;
 
 //Structure for stack
 typedef struct stack{
-    item *top;
+    StackItem *top;
 }stack;
 
 void stackInit( stack*);
