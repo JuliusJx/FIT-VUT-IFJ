ec1c2a888c5438b5011bc2eddf156a4732b168a7
JuliusJ <juliusj@DESKTOP-D10ALBB.localdomain>
2021-11-17 20:19:52 +0100

    stack.h and stack.c files created

---
+++ stack.c
@@ -0,0 +1,11 @@
+/*
+ * File: stack.c
+ * Stack for bottom-up parser and scope
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+
+
+// --End of file--
---
+++ stack.h
@@ -0,0 +1,14 @@
+/*
+ * File: stack.h
+ * Header file for stack.c
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+#ifndef stack_h_
+#define stack_h_
+
+#endif
+
+// --End of file--
