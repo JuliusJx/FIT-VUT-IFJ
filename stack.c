b00b95ed1a0e67e348dd2b7c34edc9f8c6e66787
JuliusJ <juliusj@DESKTOP-D10ALBB.localdomain>
2021-11-17 20:54:38 +0100

    stack structures and functions implemented

--- stack.c
+++ stack.c
@@ -6,6 +6,50 @@
  * Year: 2021
  */
 
+#include "stack.h"
+#include <stdio.h>
+#include <stdbool.h>
 
+//Function resets top pointer to null
+void stackInit( stack *stack){
+    stack->top = NULL;
+}
+//Function pushes item into the stack
+//Function returns true if successful, otherwise returns false
+bool stackPush( stack *stack, int value){
+    StackItem *item;
+    if((item = malloc(sizeof(StackItem))) == NULL)
+        return false;
+    item->next = stack->top;
+    item->value = value;
+    stack->top = item;
+    return true;
+}
+//Function sets target value to stack->top value if stack is not empty
+//Function returns true if successful, otherwise returns false
+bool stackTop( stack *stack, int *target){
+    if(!stackIsEmpty(stack)){
+        *target = stack->top->value;
+        return true;
+    }
+    return false;
+}
+//Function sets target value to stack->top value if stack is not empty
+//Function removes top item from stack
+//Function returns true if successful, otherwise returns false
+bool stackPop( stack *stack, int *target){
+    if(!stackIsEmpty(stack)){
+        *target = stack->top->value;
+        StackItem *tmp = stack->top;
+        stack->top = stack->top->next;
+        free(tmp);
+        return true;
+    }
+    return false;
+}
+//Function returns true if stack is empty, otherwise returns false
+bool stackIsEmpty( stack *stack){
+    return stack->top == NULL;
+}
 
 // --End of file--
--- stack.h
+++ stack.h
@@ -9,6 +9,27 @@
 #ifndef stack_h_
 #define stack_h_
 
+//Structure for items in stack
+typedef struct  item{
+    int value; //token type / scope index
+    item *next;
+}StackItem;
+
+//Structure for stack
+typedef struct stack{
+    item *top;
+}stack;
+
+void stackInit( stack*);
+
+bool stackPush( stack*, int);
+
+bool stackTop( stack*, int*);
+
+bool stackPop( stack*, int*);
+
+bool stackIsEmpty( stack*);
+
 #endif
 
 // --End of file--
