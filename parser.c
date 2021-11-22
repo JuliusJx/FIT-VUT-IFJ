c33fafe85ff92d94ef309a2ed0bd93fcf47974b7
JuliusJx <jakubjsmykal@gmail.com>
2021-11-22 15:26:54 +0100

    created parser.c and parser.h files

---
+++ parser.c
@@ -0,0 +1,12 @@
+/*
+ * File: parser.c
+ * c file for top down syntactic and semantic analysis
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+
+// --End of file--
---
+++ parser.h
@@ -0,0 +1,19 @@
+/*
+ * File: parser.h
+ * Header file for parser.c
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+#ifndef parser_h_
+#define parser_h_
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+#endif
+
+// --End of file--
