93afe12a2aeed8dfcccb72aca8139e33cbd12ffd
FNewel <m0nst3rmc@gmail.com>
2021-11-24 00:17:07 +0100

    created exp_parser.c/.h files

---
+++ exp_parser.c
@@ -0,0 +1,17 @@
+/*
+ * File: exp_parser.c
+ * c file bottom up analysis
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "exp_parser.h"
+
+
+
+// --End of file--
---
+++ exp_parser.h
@@ -0,0 +1,19 @@
+/*
+ * File: exp_parser.h
+ * Header file for exp_parser.c
+ * Subject: IFJ
+ * Authors: Jakub Julius Smykal, Ondrej Kovac, Martin Talajka, Milan Hrabovsky
+ * Year: 2021
+ */
+
+#ifndef exp_parser_h_
+#define exp_parser_h_
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+#endif
+
+// --End of file--
